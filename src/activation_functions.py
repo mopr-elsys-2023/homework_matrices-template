cdaac492bbf429d9da0f9007ca006b0ae8d9d87d
Simeon Georgiev <simeongeorgiev09@gmail.com>
2023-12-19 23:33:30 +0200

    Assignment materials

---
+++ src/activation_functions.py
@@ -0,0 +1,15 @@
+def sigmoid(matrix):
+    pass
+
+def softmax(matrix):
+    pass
+
+def tahn(matrix):
+    pass
+
+def relu(matrix):
+    return max(0, matrix)
+    pass
+
+def flatten(matrix):
+    pass
---
+++ src/forward_propagation.py
@@ -0,0 +1,4 @@
+activation_functions = {}
+
+def forward_propagation(layers):
+    pass
