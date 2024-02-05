2b1e37dee8aa9c72c8987215ede92f8d6343791b
Simeon Georgiev <simeongeorgiev09@gmail.com>
2024-02-05 14:42:50 +0200

    no asserts

--- src/activation_functions.py
+++ src/activation_functions.py
@@ -1,14 +1,26 @@
+import numpy as np
+
 def sigmoid(matrix):
-    pass
+    def sigmoid_element(x):
+        return np.exp(x) / (1 + np.exp(x))
+
+    return np.vectorize(sigmoid_element)(matrix)
 
 def softmax(matrix):
-    pass
+    return np.exp(matrix) / np.sum(np.exp(matrix), axis=0)
 
 def tanh(matrix):
-    pass
+    def tanh_element(x):
+        return ( np.exp(x) - np.exp(-x) ) / ( np.exp(x) + np.exp(-x) )
+
+    return np.vectorize(tanh_element)(matrix)
 
 def relu(matrix):
-    pass
+
+    def relu_element(x):
+        return max(x, 0)
+
+    return np.vectorize(relu_element)(matrix)
 
 def flatten(matrix):
-    pass
+    return matrix.flatten()
--- src/forward_propagation.py
+++ src/forward_propagation.py
@@ -1,4 +1,38 @@
-activation_functions = {}
+import functools
+import numpy as np
+
+from src.activation_functions import sigmoid, softmax, relu, tanh, flatten
+
+activation_functions = {
+    'sigmoid': sigmoid,
+    'softmax': softmax,
+    'relu': relu,
+    'tanh': tanh,
+    'flatten': flatten
+}
+
 
 def forward_propagation(layers):
-    pass
+
+    def apply_layer(matrix, layer):
+        activation_function_name = layer['activation']
+        activation_function = activation_functions[activation_function_name]
+
+        next_layer_matrix = None
+
+        if 'weights' not in layer and 'bias' not in layer:
+            next_layer_matrix = activation_function(matrix)
+        elif 'weights' in layer and 'bias' in layer:
+            
+            weights = layer['weights']
+            bias = layer['bias']
+
+            next_layer_matrix = np.matmul(matrix, weights)
+
+            next_layer_matrix += bias
+
+            return activation_function(next_layer_matrix)
+
+        return next_layer_matrix
+        
+    return lambda matrix: functools.reduce(apply_layer, layers, matrix)
